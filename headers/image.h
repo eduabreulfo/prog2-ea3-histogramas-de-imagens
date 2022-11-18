44e694db744ba029aecf398b5ab7548e49c76a6e
eduardoabreulfo <eduardo.abreulfo@gmail.com>
2022-11-18 14:45:08 -0300

    image prototypes

--- headers/image.h
+++ headers/image.h
@@ -1,4 +1,20 @@
 
 #pragma once
 
+#include <stdio.h>
+
+typedef enum{
+  FLOAT = 0,
+  INT = 1
+} DataType;
+
 typedef struct image Image;
+
+// inicializa uma imagem em branco na memoria
+Image *image_create();
+
+// le um arquivo .bin e retorna um ponteiro de imagem
+Image *image_read(FILE *fp);
+
+// libera a memoria da imagem e de seus componentes
+void image_destroy(Image *img);
--- src/image.c
+++ src/image.c
@@ -1,2 +1,10 @@
 
 #include "../headers/image.h"
+
+struct image
+{
+    void *pixels;
+    int n_rows;
+    int n_columns;
+    DataType type;
+};
