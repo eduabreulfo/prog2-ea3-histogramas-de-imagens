7bd768a3a0ac64b49235276d933b9a0e61af1e47
Eduardo <eduardo.abreulfo@gmail.com>
2022-11-21 21:57:45 -0300

    image_pixel function + readability

--- headers/image.h
+++ headers/image.h
@@ -17,5 +17,8 @@ Image *image_read(char *path);
 // retorna o total de pixels numa imagem
 int image_size(Image *image);
 
+// retorna o valor (inteiro) do pixel em [i/n_rows][i%n_columns]
+int image_pixel(Image *image, int i);
+
 // libera a memoria da imagem e de seus componentes
 void image_destroy(Image *image);
--- src/image.c
+++ src/image.c
@@ -18,18 +18,23 @@ Image *image_read(char *path)
         exit(1);
     }
 
-    fread(&image->n_rows, 4, 1, fp);
-    fread(&image->n_columns, 4, 1, fp);
+    const int N_BYTES = 4;
 
-    fread(&image->type, 4, 1, fp);
+    fread(&image->n_rows, N_BYTES, 1, fp);
+    fread(&image->n_columns, N_BYTES, 1, fp);
 
-    image->pixels = calloc((image->n_rows)*(image->n_columns), 4);
-    fread(image->pixels, 4, (image->n_rows)*(image->n_columns), fp);
+    fread(&image->type, N_BYTES, 1, fp);
 
-    if(image->type == FLOAT){
+    image->pixels = calloc((image->n_rows)*(image->n_columns), N_BYTES);
+
+    fread(image->pixels, N_BYTES, (image->n_rows)*(image->n_columns), fp);
+
+    if(image->type == FLOAT)
+    {
         int i=0;
-        for(i=0;i<image_size(image);i++){
-            *(int*)(image->pixels + i*4) = ((*(int*)(image->pixels + i*4))*255);
+        for(i=0;i<image_size(image);i++)
+        {
+            *(int*)(image->pixels + i*sizeof(int)) = (*(int*)(image->pixels + i*sizeof(int)))*255;
         }
         image->type = INT;
     }
@@ -41,6 +46,14 @@ Image *image_read(char *path)
 int image_size(Image *image)
 {
     return (image->n_rows)*(image->n_columns);
+}
+
+int image_pixel(Image *image, int i)
+{
+    if(i > image_size(image)){
+        printf("Acesso invalido!\n");
+    }
+    return *(int*)(image->pixels + i*sizeof(int));
 }
 
 void image_destroy(Image *image)
